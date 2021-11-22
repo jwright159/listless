3a3de23893bfc2bd5f28195e07a69d8707be2a34
Jade Wright <iamjwright159@gmail.com>
2021-11-22 10:58:55 -0500

    Initial commit

---
+++ src/main/java/com/github/jwright159/listless/Listless.java
@@ -0,0 +1,27 @@
+package com.github.jwright159.listless;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Listless.MODID, name = Listless.NAME, version = Listless.VERSION)
+public class Listless
+{
+	public static final String MODID = "minestuckgodtier";
+	public static final String NAME = "Minestuck God Tier";
+	public static final String VERSION = "@VERSION@";
+
+	@Mod.Instance(MODID)
+	public static Listless instance;
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		Logger logger = LogManager.getLogger(MODID);
+		logger.info("Logging items");
+		for (Item item : Item.REGISTRY)
+			logger.info(Item.REGISTRY.getIDForObject(item) + " " + item.getRegistryName());
+	}
+}
